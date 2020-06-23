1184ce648979b9c5a8f9af34f61c0a65d9793376
taeeun kim <kimtaeeeny@gmail.com>
2020-06-23 15:24:38 +0900

    chore : add react-app-rewired to demo

--- examples/demo-cra/src/App.tsx
+++ examples/demo-cra/src/App.tsx
@@ -6,7 +6,7 @@ import Grid from "./components/Grid";
 import GridColor from "./components/GridColor";
 import PixelType from "./components/PixelType";
 import DownloadButton from "./components/DownloadButton";
-import { Pixelator } from "interactive-pixelator";
+import { Pixelator } from "../../../../interactive-pixelator";
 
 const App = () => {
   const [pixelSize1, setPixelSize1] = useState(100);
